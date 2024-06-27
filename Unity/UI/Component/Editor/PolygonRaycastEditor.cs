4518b5ca21b49c8275266bce6468d665bf165916
YunXiaoGu <3038949117@qq.com>
2024-06-27 21:10:12 +0800

    新增Unity PolygonRaycast组件

---
+++ Unity/UI/Component/Editor/PolygonRaycastEditor.cs
@@ -0,0 +1,227 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace UI.Component.Editor
+{
+    [CustomEditor(typeof(PolygonRaycast))]
+    [CanEditMultipleObjects]
+    public class PolygonRaycastEditor : Editor
+    {
+        private PolygonRaycast component;
+
+        private bool canEditCollider;
+        private List<bool> intersects;
+
+        private void OnEnable()
+        {
+            component = (PolygonRaycast)target;
+            intersects = new List<bool>();
+        }
+
+        private void OnDisable()
+        {
+            canEditCollider = false;
+            intersects.Clear();
+            intersects = null;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawEditCollider();
+            base.OnInspectorGUI();
+        }
+
+        private void OnSceneGUI()
+        {
+            DrawCollider();
+        }
+
+        // 绘制编辑碰撞体按钮
+        private void DrawEditCollider()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Edit Collider", GUILayout.Width(80));
+            GUI.backgroundColor = canEditCollider ? Color.green : Color.white;
+            if (GUILayout.Button("Edit", GUILayout.Width(60)))
+            {
+                canEditCollider = !canEditCollider;
+                if (canEditCollider)
+                {
+                    ToolManager.SetActiveTool<NoneTool>();
+                    Selection.activeGameObject = component.gameObject;
+                }
+                else
+                {
+                    ToolManager.RestorePreviousTool();
+                }
+            }
+
+            GUI.backgroundColor = Color.white;
+            if (canEditCollider)
+            {
+                if (GUILayout.Button("+", GUILayout.Width(30)))
+                {
+                    Undo.RecordObject(component, "Edit PolygonRaycast Point");
+                    Vector2[] points = component.points;
+                    if (points == null)
+                    {
+                        points = new Vector2[3];
+                    }
+                    else
+                    {
+                        Vector2[] newPoints = new Vector2[points.Length + 1];
+                        for (int i = 0; i < points.Length; i++)
+                        {
+                            newPoints[i] = points[i];
+                        }
+
+                        points = newPoints;
+                    }
+
+                    component.points = points;
+                }
+
+                if (GUILayout.Button("-", GUILayout.Width(30)))
+                {
+                    Undo.RecordObject(component, "Edit PolygonRaycast Point");
+                    Vector2[] points = component.points;
+                    if (points != null && points.Length > 3)
+                    {
+                        Vector2[] newPoints = new Vector2[points.Length - 1];
+                        for (int i = 0; i < newPoints.Length; i++)
+                        {
+                            newPoints[i] = points[i];
+                        }
+                        component.points = newPoints;
+                    }
+                }
+
+                if (GUILayout.Button("□", GUILayout.Width(30)))
+                {
+                    // 将RectTransform的四个角位置赋值给points
+                    Undo.RecordObject(component, "Edit PolygonRaycast Points");
+                    Vector2[] points = new Vector2[4];
+                    RectTransform rectTransform = component.rectTransform;
+                    Vector2 size = rectTransform.sizeDelta;
+                    Vector2 pivot = rectTransform.pivot;
+                    Vector2 offset = new Vector2(size.x * pivot.x, size.y * pivot.y);
+                    points[0] = new Vector2(-offset.x, -offset.y);
+                    points[1] = new Vector2(-offset.x, size.y - offset.y);
+                    points[2] = new Vector2(size.x - offset.x, size.y - offset.y);
+                    points[3] = new Vector2(size.x - offset.x, -offset.y);
+                    component.points = points;
+                }
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // 绘制碰撞体
+        private void DrawCollider()
+        {
+            if (component.points == null || component.points.Length < 3)
+            {
+                return;
+            }
+
+            Vector2[] points = component.points;
+
+            intersects.Clear();
+            for (int i = 0; i < points.Length; i++)
+            {
+                intersects.Add(false);
+            }
+
+            // 检查相交线段，标记相交的线段
+            for (int i = 0; i < points.Length; i++)
+            {
+                Vector2 p1 = component.transform.TransformPoint(points[i]);
+                Vector2 p2 = component.transform.TransformPoint(points[(i + 1) % points.Length]);
+
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    Vector2 q1 = component.transform.TransformPoint(points[j]);
+                    Vector2 q2 = component.transform.TransformPoint(points[(j + 1) % points.Length]);
+
+                    if (IsIntersect(p1, p2, q1, q2))
+                    {
+                        intersects[i] = true;
+                        intersects[j] = true;
+                    }
+                }
+            }
+
+            var originColor = Handles.color;
+            // 绘制多边形
+            for (int i = 0; i < points.Length; ++i)
+            {
+                Vector2 worldPoint = component.transform.TransformPoint(points[i]);
+                Vector2 nextWorldPoint = component.transform.TransformPoint(points[(i + 1) % points.Length]);
+                Handles.color = intersects[i] ? Color.red : Color.green;
+                Handles.DrawLine(worldPoint, nextWorldPoint);
+                // Handles.DrawAAPolyLine(1f, new Vector3[] { worldPoint, nextWorldPoint });
+
+                if (canEditCollider)
+                {
+#if UNITY_2022_1_OR_NEWER
+                    Vector2 newWorldPoint = Handles.FreeMoveHandle(worldPoint, HandleUtility.GetHandleSize(component.transform.position) * 0.05f, Vector3.zero, Handles.DotHandleCap);
+#else
+                    Vector2 newWorldPoint = Handles.FreeMoveHandle(worldPoint, Quaternion.identity, HandleUtility.GetHandleSize(component.transform.position) * 0.05f, Vector3.zero, Handles.DotHandleCap);
+#endif
+                    if (worldPoint != newWorldPoint)
+                    {
+                        EditorUtility.SetDirty(component);
+                        Undo.RecordObject(component, "Edit PolygonRaycast Collider");
+                        points[i] = component.transform.InverseTransformPoint(newWorldPoint);
+                        component.points = points;
+                    }
+
+                    // 操作点上的序号，大一点
+                    Handles.Label(worldPoint, i.ToString(), new GUIStyle()
+                    {
+                        fontSize = 20,
+                        normal = new GUIStyleState() { textColor = Color.red }
+                    });
+                }
+            }
+
+            Handles.color = originColor;
+        }
+        
+        /// <summary>
+        /// 叉乘
+        /// </summary>
+        float Cross(Vector2 a, Vector2 b)
+        {
+            return a.x * b.y - b.x * a.y;
+        }
+
+        /// <summary>
+        /// 判断AB与CD两条线段是否相交
+        /// </summary>
+        bool IsIntersect(Vector2 a, Vector2 b, Vector2 c, Vector2 d)
+        {
+            //以线段ab为准，是否c，d在同一侧
+            Vector2 ab = b - a;
+            Vector2 ac = c - a;
+            Vector2 ad = d - a;
+            if (Cross(ab, ac) * Cross(ab, ad) >= 0)
+            {
+                return false;
+            }
+
+            //以线段cd为准，是否ab在同一侧
+            Vector2 cd = d - c;
+            Vector2 ca = a - c;
+            Vector2 cb = b - c;
+            if (Cross(cd, ca) * Cross(cd, cb) >= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Unity/UI/Component/PolygonRaycast.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Component
+{
+    /// <summary>
+    /// 不规则点击区域组件
+    /// <para>与Empty4Raycast组件不同的是，该组件可以和Button组件组合使用，实现不规则点击区域</para>
+    /// </summary>
+    public class PolygonRaycast : MaskableGraphic, ICanvasRaycastFilter
+    {
+        public Vector2[] points = new Vector2[0];
+
+        protected override void OnPopulateMesh(VertexHelper toFill)
+        {
+            toFill.Clear();
+        }
+
+        public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+        {
+            if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, sp, eventCamera, out var local))
+            {
+                return false;
+            }
+
+            return OverlapPoint(local, points);
+        }
+
+        /// <summary>
+        /// 点是否在多边形内
+        /// </summary>
+        /// <para>使用射线法</para>
+        /// <remarks>
+        /// 如果多边形有重叠区域，则重叠区域是否算作在多边形内取决于规则
+        ///     奇-偶规则（Odd-even Rule）
+        ///     非零环绕数规则（Nonzero Winding Number Rule）
+        /// </remarks>
+        bool OverlapPoint(Vector2 point, Vector2[] polygon)
+        {
+            if (polygon.Length < 3)
+            {
+                return false;
+            }
+
+            int j = polygon.Length - 1;
+            bool oddNodes = false;
+            for (int k = 0; k < polygon.Length; k++)
+            {
+                Vector2 point1 = polygon[k];
+                Vector2 point2 = polygon[j];
+                if ((point1.y > point.y != point2.y > point.y) && (point.x < (point2.x - point1.x) * (point.y - point1.y) / (point2.y - point1.y) + point1.x))
+                {
+                    oddNodes = !oddNodes;
+                }
+
+                j = k;
+            }
+
+            return oddNodes;
+        }
+    }
+}
